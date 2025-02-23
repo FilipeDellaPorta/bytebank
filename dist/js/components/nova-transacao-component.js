3361ef0017badc688bcc3d83292b5e04e7e1cd8a
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-23 13:05:11 -0300

    refactor: :recycle: refactor: paradigma funcional para orientado a objetos

--- dist/js/components/nova-transacao-component.js
+++ dist/js/components/nova-transacao-component.js
@@ -1,4 +1,4 @@
-import Conta from "../types/Conta.js";
+import Conta from "../types/Conta-antiga.js";
 import ExtratoComponent from "./extrato-component.js";
 import SaldoComponent from "./saldo-component.js";
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
